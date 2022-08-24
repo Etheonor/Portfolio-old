9662d9fb1530c19d408762eecda5d675a06ffb80
Michael <etheanore@gmail.com>
2022-08-24 16:55:27 +0200

    Fix centered text + projects display on small screen

--- components/Project.tsx
+++ components/Project.tsx
@@ -12,15 +12,15 @@ interface ProjectProps {
 
 const Project = (props: ProjectProps): JSX.Element => (
   <section
-    className={`flex h-screen flex-row flex-wrap ${
+    className={`flex lg:h-screen flex-row flex-wrap ${
       props.bg === 'dark'
         ? 'bg-stone-800 text-slate-100'
         : 'bg-slate-100 text-stone-800'
     }`}>
-    <div className="flex flex-col m-auto">
+    <div className="flex flex-col m-auto p-8 lg:p-0 ">
       <div className="m-auto">
         <div className="m-auto font-body text-base md:text-xl xl:text-2xl">
-          <div className="flex flex-col max-w-md">
+          <div className="flex flex-col flex-grow: 1">
             <h2 className="text-5xl xl:text-7xl 2xl:text-9xl m-auto font-title">
               {props.project}
             </h2>
@@ -29,7 +29,7 @@ const Project = (props: ProjectProps): JSX.Element => (
                 props.bg === 'dark' ? 'border-slate-100' : 'border-stone-900'
               }`}
             />
-            <p className="max-w-xl px-5 text-center md:leading-relaxed lg:px-0 2xl:text-base xl:text-sm text-xs">
+            <p className="max-w-md px-5 text-center md:leading-relaxed lg:px-0 2xl:text-base xl:text-sm text-xs m-auto">
               {props.description}
             </p>
           </div>
